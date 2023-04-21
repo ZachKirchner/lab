10e60dd3f336a5fabe10fbb7896e18d1c183c34d
ExplodingPurpur <purpurillar@gmail.com>
2023-04-21 18:53:11 -0500

    Added docstrings and type hinting to account.py

--- account.py
+++ account.py
@@ -1,9 +1,21 @@
 class Account:
-    def __init__(self, name):
+    def __init__(self, name: str):
+        """
+        This function initializes the account
+
+        :param name: This is the name of the account
+        """
         self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount: float):
+        """
+        This function adds a value to the account balance then returns True if added successfully and False if it
+        failed.
+
+        :param amount: This is the value to be added
+        :return: A boolean value representing whether the addition succeeded or failed
+        """
         if amount > 0:
             pass
         else:
@@ -11,7 +23,14 @@ class Account:
         self.__account_balance += amount
         return True
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float):
+        """
+        This functions subtracts a vlue from the account balance then returns True if subtracted successfully and
+        False if it failed.
+
+        :param amount: This is the value to be subtracted
+        :return: A boolean value representing whether the subtraction succeeded or failed
+        """
         if amount > self.__account_balance or amount <= 0:
             return False
         else:
@@ -19,7 +38,17 @@ class Account:
             return True
 
     def get_balance(self):
+        """
+        This functions returns the account balance.
+
+        :return: The account balance
+        """
         return self.__account_balance
 
     def get_name(self):
+        """
+        This function returns the name of the account.
+
+        :return: The name of the account
+        """
         return self.__account_name
